f28c405ea04b7f544e6e2c988b86e61a1477f27a
tohami <tohami1993@gmail.com>
2021-06-15 00:09:26 +0200

    Revert "Update main.dart"
    This reverts commit a6c15ffb

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'my_app.dart';
 
 void main() async{
-  //this is a commit from github
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
