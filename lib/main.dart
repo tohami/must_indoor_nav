b2ace30672eb7311edd089b3abea8784efeadddd
tohami <tohami1993@gmail.com>
2021-06-14 23:59:05 +0200

    change the commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'my_app.dart';
 
 void main() async{
-  //this is a commit from github
+  //this is a commit from android
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
