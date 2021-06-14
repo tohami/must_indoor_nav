a6c15ffb5cb5899eddc1f86ccff791a93fde6cf9
tohami hussin <tohami1993@gmail.com>
2021-06-14 23:58:19 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'my_app.dart';
 
 void main() async{
+  //this is a commit from github
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
